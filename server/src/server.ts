bbb37be1c6b02c6c4ab8b0ce5898683534bc50b3
PedroNnsM <pedronunes95@outlook.com>
2022-09-14 13:54:24 -0300

    Db games

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,40 @@
-import express from "express";
+import express, { response } from "express";
 
 
 const app = express()
 
-app.get('/ads', (request, response) => {
-    return response.json([
-        { id: 1, name: 'Anuncio 1'},
-        { id: 2, name: 'Anuncio 2'},
-        { id: 3, name: 'Anuncio 3'},
-        { id: 4, name: 'Anuncio 4'},
-    ])
+/*
+    parms
+
+* Query : sao parametros vindo dos ? ex: localhost:3333/ads?page=2 (quando precisa presistir estado)- ficam disponiveis na url
+
+* Route : sao paramentros vindo por rotas ex localhost:3333/post/como-criar-uma-api-em-node
+
+* Body : enviar varias informacoes numa unica requisicao (normalmente em envio de formulario)
+*/
+
+app.get('/games', (request, response) =>{
+    return response.json([])
+});
+
+// http codes iniciais com 
+// 200 = sucesso
+// 300 = redirecionamento
+// 400 = erros por algum codigo
+// 500 = erros inesperados 
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
 })
+
+app.get('/games/:id/ads', (request, response) => {
+    const adId = request.params.id;
+
+    return response.json([])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+
+})
+
 
 app.listen(3333)
---
+++ server/text.md
@@ -0,0 +1,31 @@
+### Entidade   
+
+
+### Game
+
+id
+title
+bannerURL
+
+
+CDN (Amazons3)
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+### Casos de uso 
+
+- Listagem de games com contagem de anuncios
+- Criacao de novos anuncios
+- Listagem de anuncios por game
+- Buscar discord pelo anuncio
