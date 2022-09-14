cfdefce70b779184b6d34d757cbb07d213edbc28
PedroNnsM <pedronunes95@outlook.com>
2022-09-13 23:39:09 -0300

    loading mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,55 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+import {  StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
 
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     //view = div
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Hello world!
-        </Text>
-
-        <Button title="send 1" />
-        <Button title="send 2" />
-        <Button title="send 3"  />
-        <Button title="send 4" />
-        <Button title="send 5" />
-        
-      <StatusBar style="auto" />
-    </View>
+    <Background >
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#c5c5c5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    color: '#fff',
-    fontSize: 22,
-    backgroundColor: '#000',
-    padding: 15,
-    borderRadius: 35,
-  },
-});
